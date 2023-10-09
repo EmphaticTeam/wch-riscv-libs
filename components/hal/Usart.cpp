1e637a9561191e0206c2d3ac32e09b2880e5f6e8
Medan <medan.dev@ya.ru>
2023-10-09 19:48:42 +0400

    Usart class done

--- components/hal/Usart.cpp
+++ components/hal/Usart.cpp
@@ -1,38 +1,41 @@
 ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
 /// @file Usart.cpp
 /// @author {authors}
-/// @brief 
+/// @brief
 /// @date 2023-10-05
-/// 
+///
 /// Copyright (c) 2023
-/// 
+///
 
 #include "Usart.hpp"
 
-#define TX_TIMEOUT  10000
+#define TX_TIMEOUT 10000
 
 namespace ch32v_lib::interfaces
 {
     ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
-    /// @brief 
-    /// 
-    /// @param[inst]
-    /// @param[baudRate]
+    /// @brief USART Initialization
+    ///
+    /// Initializes the USART instance with the specified parameters.
+    ///
+    /// @param[inst] Pointer to the USART peripheral instance.
+    /// @param[baudRate] The baud rate for USART communication.
     ///
     void Usart::Init(USART_TypeDef *inst, u32 baudrate)
     {
         instance = inst;
-        // instance->CTLR1 &= ~USART_CTLR1_UE;
         instance->CTLR1 |= USART_CTLR1_UE;
-        instance->CTLR1 |= USART_CTLR1_RE | USART_CTLR1_TE | USART_CTLR1_RXNEIE;
+        instance->CTLR1 |= USART_CTLR1_RE | USART_CTLR1_TE | USART_CTLR1_RXNEIE | USART_CTLR1_IDLEIE;
         instance->CTLR3 |= USART_CTLR3_EIE;
         SetBaudrate(baudrate);
     }
 
     ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
-    /// @brief 
-    /// 
-    /// @param[baudrate]
+    /// @brief Set USART Baudrate
+    ///
+    /// Sets the baud rate for USART communication.
+    ///
+    /// @param[baudrate] The desired baud rate.
     ///
     void Usart::SetBaudrate(u32 baudrate)
     {
@@ -43,77 +46,118 @@ namespace ch32v_lib::interfaces
     }
 
     ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
-    /// @brief 
-    /// 
-    /// @param[msg]
-    /// @param[len]
-    /// @return int 
+    /// @brief Send Message via USART
+    ///
+    /// Sends a message through USART with the specified length.
+    ///
+    /// @param[msg] Pointer to the message buffer.
+    /// @param[len] Length of the message.
+    /// @return int The number of bytes sent.
     ///
     int Usart::SendMessage(const u8 *msg, const u32 len)
     {
         u32 i = 0;
-        for(; i < len; i++)
+        for (; i < len; i++)
         {
-            if(WaitFlag(USART_STATR_TXE, SET, TX_TIMEOUT) != READY) return i;
+            if (WaitFlag(USART_STATR_TXE, SET, TX_TIMEOUT) != READY)
+                return i;
             instance->DATAR = msg[i];
         }
         return i;
     }
 
     ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
-    /// @brief 
-    /// 
-    /// @param[count]
-    /// @return ErrorStatus 
+    /// @brief Receive Data with Interrupts
+    ///
+    /// Prepares to receive a specified number of bytes via USART interrupts.
+    ///
+    /// @param[count] The expected number of bytes to receive.
+    /// @return ErrorStatus READY if the operation can be performed, NoREADY otherwise.
     ///
     ErrorStatus Usart::ReceiveIT(u16 count)
     {
-        if(count > USART_RX_BUF_LEN) return NoREADY;
-        _currentCounter = 0;
+        if (count > USART_RX_BUF_LEN)
+            return NoREADY;
         _expectCounter = count;
         return READY;
     }
 
     ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
-    /// @brief 
-    /// 
+    /// @brief Receive Data Until Idle
     ///
-    void Usart::InterruptHandler()
+    /// Prepares to receive data until the USART line goes idle.
+    ///
+    /// @return ErrorStatus READY if the operation can be performed, NoREADY otherwise.
+    ///
+    ErrorStatus Usart::ReceiveUntilIdle()
     {
-        if((instance->STATR & USART_STATR_RXNE) != 0)
-        {
-            _rxBuffer[_currentCounter++] = (uint16_t)(instance->DATAR & (uint16_t)0x01FF);
-            if((_currentCounter == _expectCounter) && rxCallback) 
-            {
-                rxCallback(this, _rxBuffer, _currentCounter);
-                _currentCounter++;
-            }
-        }
-        instance->STATR = 0;
+        _currentCounter = 0;
+        _untilIdle = true;
+        return READY;
     }
 
     ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
-    /// @brief 
-    /// 
-    /// @param[flag]
-    /// @param[expectState]
-    /// @param[timeout]
-    /// @return ErrorStatus 
+    /// @brief USART Interrupt Handler
+    ///
+    /// Handles USART interrupts and processes received data.
+    ///
+    void Usart::InterruptHandler()
+    {
+        u32 len = 0;
+
+        if (READ_BIT(instance->STATR, USART_STATR_RXNE) != 0)
+        {
+            _rxBuffer[_currentCounter] = (u16)(instance->DATAR & (u16)0x01FF);
+            _currentCounter++;
+
+            if (_currentCounter >= USART_RX_BUF_LEN || (!_untilIdle && (_currentCounter == _expectCounter)))
+            {
+                len = _currentCounter;
+                _currentCounter = 0;
+                if (rxCallback != 0)
+                    rxCallback(_rxBuffer, len);
+            }
+        }
+        else if (READ_BIT(instance->STATR, USART_STATR_IDLE) != 0)
+        {
+            _rxBuffer[_currentCounter] = (u16)(instance->DATAR & (u16)0x01FF);
+            _currentCounter++;
+
+            if (_untilIdle)
+            {
+                len = _currentCounter;
+                _currentCounter = 0;
+                _untilIdle = false;
+                if (rxCallback != 0)
+                    rxCallback(_rxBuffer, len);
+            }
+        }
+    }
+
+    ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
+    /// @brief Wait for USART Flag
+    ///
+    /// Waits for a specific USART flag to reach the expected state with a timeout.
+    ///
+    /// @param[flag] The USART flag to check.
+    /// @param[expectState] The expected state of the flag (SET or RESET).
+    /// @param[timeout] The timeout in number of iterations.
+    /// @return ErrorStatus READY if the flag reaches the expected state, NoREADY otherwise.
     ///
     ErrorStatus Usart::WaitFlag(u16 flag, FlagStatus expectState, u32 timeout)
     {
-        __IO uint32_t _counter = 0;
+        __IO u32 _counter = 0;
         FlagStatus flagStatus = RESET;
-        
+
         do
         {
             flagStatus = ((instance->STATR & flag) != 0) ? SET : RESET;
-            _counter++;  
-        } while((_counter < timeout) && (flagStatus != expectState));
-        
-        if (flagStatus == expectState) 
+            _counter++;
+        } while ((_counter < timeout) && (flagStatus != expectState));
+
+        if (flagStatus == expectState)
             return READY;
-        else 
+        else
             return NoREADY;
     }
 }
--- components/hal/Usart.hpp
+++ components/hal/Usart.hpp
@@ -1,51 +1,55 @@
 ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
 /// @file Usart.hpp
 /// @author {authors}
-/// @brief 
+/// @brief
 /// @date 2023-10-05
-/// 
+///
 /// Copyright (c) 2023
-/// 
+///
 
 #ifndef __USART__H__
 #define __USART__H__
 
-#include <functional>
 #include "Common.h"
 
-#define USART_TX_BUF_LEN    256
-#define USART_RX_BUF_LEN    256
+#define USART_TX_BUF_LEN 256
+#define USART_RX_BUF_LEN 256
 
 namespace ch32v_lib::interfaces
 {
     class Usart;
-    typedef void (*UsartRxCallback)(Usart* obj, u8* data, u32 len);
+    typedef void (*UsartRxCallback)(u8 *data, u32 len);
 
     ///=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=///
-    /// @brief 
-    /// 
+    /// @brief USART Communication Class
+    ///
+    /// This class provides functionality for USART (Universal Synchronous/Asynchronous Receiver/Transmitter) communication.
+    ///
     class Usart
     {
     public:
-        USART_TypeDef* instance = nullptr;
+        USART_TypeDef *instance = nullptr;
         UsartRxCallback rxCallback;
 
         void Init(USART_TypeDef *inst, u32 baudrate);
         void SetBaudrate(u32 baudrate);
-        int SendMessage(const u8* msg, const u32 len);
+        int SendMessage(const u8 *msg, const u32 len);
         // void SendMessageIT(u8* msg, u32 len);
         // void SendMessageDMA(u8* msg, u32 len);
 
         ErrorStatus ReceiveIT(u16 count);
+        ErrorStatus ReceiveUntilIdle();
         void InterruptHandler();
+
     private:
         u8 _txBuffer[USART_TX_BUF_LEN];
         u8 _rxBuffer[USART_RX_BUF_LEN];
         u16 _currentCounter = 0;
         u16 _expectCounter = 0;
+        bool _untilIdle = false;
 
         ErrorStatus WaitFlag(u16 flag, FlagStatus expectState, u32 timeout);
     };
 }
 
-#endif  //!__USART__H__
+#endif //!__USART__H__
