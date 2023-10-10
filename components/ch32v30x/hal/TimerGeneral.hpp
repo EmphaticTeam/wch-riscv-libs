ff8d9a9f80bd2f7001bbc5c10b4808008d5dea48
Medan <medan.dev@ya.ru>
2023-10-11 01:05:34 +0400

    components restructurized

---
+++ components/ch32v30x/hal/TimerGeneral.hpp
@@ -0,0 +1,18 @@
+#ifndef __TIMERGENERAL__H__
+#define __TIMERGENERAL__H__
+
+#include "Common.h"
+#include "TimerBasic.hpp"
+
+namespace ch32v_lib::timers
+{
+    class TimerGeneral : TimerBasic
+    {
+    public:
+
+    private:
+
+    };
+}
+
+#endif  //!__TIMERGENERAL__H__
--- components/hal/TimerBasic.cpp
+++
@@ -1,9 +0,0 @@
-#include "TimerBasic.hpp"
-
-namespace ch32v_lib::timers
-{
-    void TimerBasic::Init(TIM_TypeDef* tim)
-    {
-        timer = tim;
-    }
-}
