5ae56a923c053ef48f1353d8cdca23a6eaf145e8
Medan <medan.develop@gmail.com>
2023-10-19 23:56:18 +0400

    TimerGeneral backup

--- components/ch32v30x/hal/HAL_TimerGeneral.cpp
+++ components/ch32v30x/hal/HAL_TimerGeneral.cpp
@@ -2,5 +2,37 @@
 
 namespace ch32v_lib::timers
 {
-    
+    void TimerGeneral::OutputConfigure(u8 ch, ChMode mode, State preload, State fast)
+    {
+        u16* reg;
+        u16 shift = ((ch-1) % 2) * 8;
+
+        if(ch == 1 || ch == 2)
+            reg = (u16*)&timer->CHCTLR1;
+        else if(ch == 3 || ch == 4)
+            reg = (u16*)&timer->CHCTLR1;
+        else return;
+
+        auto temp = *reg;
+        temp &= ~(0xFF << shift);
+        temp |= ((u16)mode << 4) | ((u16)preload << 3) | ((u16)fast << 2);
+        *reg = temp;
+    }
+
+    void TimerGeneral::OutputState(u8 ch, State state)
+    {
+        if(ch < 1 || ch > 4) return;
+        u16 shift = (ch-1) * 4;
+
+        auto temp = timer->CCER;
+        temp &= ~(0x1 << shift);
+        temp |= ((u16)state << shift);
+        timer->CCER = temp;
+    }
+
+    void TimerGeneral::CompareSet(u8 ch, u16 value)
+    {
+        if(ch < 1 || ch > 4) return;
+        ((u32*)&timer->CH1CVR)[ch-1] = value;
+    }
 }
--- components/ch32v30x/hal/HAL_TimerGeneral.hpp
+++ components/ch32v30x/hal/HAL_TimerGeneral.hpp
@@ -6,10 +6,30 @@
 
 namespace ch32v_lib::timers
 {
-    class TimerGeneral : TimerBasic
+    enum class ChMode : u8
+    {
+        Frozen = 0x0,
+        Active = 0x1,
+        Inactive = 0x2,
+        Overturn = 0x3,
+        ActiveForce = 0x4,
+        InactiveForce = 0x5,
+        PWM1 = 0x6,
+        PWM2 = 0x7
+    };
+
+    enum class State : u8
+    {
+        Disable = 0x0,
+        Enable = 0x1
+    };
+
+    class TimerGeneral : public TimerBasic
     {
     public:
-
+        void OutputConfigure(u8 ch, ChMode mode, State preload, State fast);
+        void OutputState(u8 ch, State state);
+        void CompareSet(u8 ch, u16 value);
     private:
 
     };
